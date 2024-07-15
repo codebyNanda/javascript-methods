799b39011ada9531fed550c0f91527ac1583bd0c
Ananda Ferreira <ananda.ferreira@raizen.com>
2024-07-15 20:31:07 -0300

    feat: for method example

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Exemplo método for of
+let heavyMetalBands = ['Judas Priest', 'Slayer', 'Velho', 'Bathory']
+
+for (const band of heavyMetalBands) {
+    if (band) {
+        console.log(band)
+    }
+}
